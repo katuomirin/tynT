3d09d134051440382c29909aeb827aed4cf4c689
katuomirin <2003euph@gmail.com>
2023-11-07 10:04:49 +0900

    Update create.php

--- shoppingsite/create/create.php
+++ shoppingsite/create/create.php
@@ -1,4 +1,28 @@
 <?php require '../headerhome.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tシャツ作成手順</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>オリジナルTシャツ作成サイト</h1>
+        <nav>
+            <ul>
+                <li><a href="create_design.php">デザインを作成</a></li>
+                <li><a href="view_cart.php">カートを見る</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <!-- ここにコンテンツを追加 -->
+    </main>
+    <footer>ssssss
+        &copy; <?php echo date("Y"); ?> オリジナルTシャツ作成サイト
+    </footer>
+</body>
+</html>
 
 
 
