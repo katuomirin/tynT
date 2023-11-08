14e70153adc0b2503ebd19b913d0cfcc2591eae3
yamasita123 <2201209@s.asojuku.ac.jp>
2023-11-08 14:32:23 +0900

    Merge branch 'main' of https://github.com/katuomirin/tynT

--- shoppingsite/reji.php
+++ shoppingsite/reji.php
@@ -1,16 +1,22 @@
 <?php require 'headerhome.php'; ?>
 <?php
         echo '<form action="reji2.php" method="post">';
-        echo '<p><h3>名前(漢字)<h3></P>';
-        echo '<p><input type="text"  name="aaa" ></p>';
-        echo '<p><h3>名前(ナマエ)<h3></P>';
-        echo '<p><input type="text"  name="kana" placeholder="ナマエ"></p>';
-
-        echo '<p><h3>お届け先住所<h3></p>';
-        echo '<p><input type="text"  name="bangou" placeholder="郵便番号"> <input type="submit" value="住所検索"></p>';
-        echo '<p><input type="password"  name="password" ></p>';
-        echo '<p><input type="submit" value="確定"></p>';
-        echo '<p><input type="password"  name="password" ></p>';
-
+        echo '<table>';
+        echo '<tr><td>名前(漢字)</td><td>';
+        echo '<input type="text" name="name">';
+        echo '</td></tr>';
+        echo '<tr><td>名前(カナ)</td><td>';
+        echo '<input type="text"  name="kana">';
+        echo '</td></tr>';
+        echo '<tr><td>お届け先住所</td><td>';
+        echo '<input type="text"  name="yuubin">郵便番号';
+        echo '<input type="text"  name="zyuusho">';
+        echo '</td></tr>';
+        echo '<tr><td>電話番号</td><td>';
+        echo '<input type="text" name="ban">';
+        echo '</td></tr>';
+        echo '</table>';
+        echo '<input type="submit" value="確定">';
+        echo '</from>';
     ?>
 <?php require 'footer.php'; ?>
