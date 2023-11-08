6db84a7617ae05863550f9808c68580ccae4c2bc
yamasita123 <2201209@s.asojuku.ac.jp>
2023-11-08 14:19:25 +0900

    Update reji.php

--- shoppingsite/reji.php
+++ shoppingsite/reji.php
@@ -1,17 +1,16 @@
 <?php require 'headerhome.php'; ?>
 <?php
         echo '<form action="reji2.php" method="post">';
-        echo '<input type="text"  name="nnnnn" >';
-        echo '<h3>名前(漢字)<h3>';
-        echo '<input type="text"  name="aaa" >';
-        echo '<h3>名前(ナマエ)<h3>';
-        echo '<input type="text"  name="kana" placeholder="ナマエ">';
+        echo '<p><h3>名前(漢字)<h3></P>';
+        echo '<p><input type="text"  name="aaa" ></p>';
+        echo '<p><h3>名前(ナマエ)<h3></P>';
+        echo '<p><input type="text"  name="kana" placeholder="ナマエ"></p>';
 
-        echo '<h3>お届け先住所<h3>';
-        echo '<input type="text"  name="bangou" placeholder="郵便番号"> <input type="submit" value="住所検索">';
-        echo '<input type="password"  name="password" >';
-        echo '<input type="submit" value="確定">';
-        echo '<input type="password"  name="password" >';
+        echo '<p><h3>お届け先住所<h3></p>';
+        echo '<p><input type="text"  name="bangou" placeholder="郵便番号"> <input type="submit" value="住所検索"></p>';
+        echo '<p><input type="password"  name="password" ></p>';
+        echo '<p><input type="submit" value="確定"></p>';
+        echo '<p><input type="password"  name="password" ></p>';
 
     ?>
 <?php require 'footer.php'; ?>
