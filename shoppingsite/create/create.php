428b64d92b22c05218ef36f8c430feb23c3ac715
katuomirin <2003euph@gmail.com>
2023-11-08 11:30:53 +0900

    Update create.php

--- shoppingsite/create/create.php
+++ shoppingsite/create/create.php
@@ -1,9 +1,9 @@
-<?php require '../headerhome.php'; ?>
+<?php require '../header.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Tシャツ作成手順</title>
-    <link rel="stylesheet" type="text/css" href="./css/create.css">
+    <link rel="stylesheet" href="../css/create.css">
 </head>
 <body>
     <header>
@@ -16,17 +16,62 @@
         </nav>
     </header>
     <main>
-        <div class="flow-chart">
-            <ul>
-                <li><a href="step1.php">ステップ 1: デザインを選択</a></li>
-                <li><a href="step2.php">ステップ 2: サイズを選択</a></li>
-                <li><a href="step3.php">ステップ 3: カスタマイズオプションを選択</a></li>
-                <li><a href="step4.php">ステップ 4: デザインをプレビュー</a></li>
-                <li><a href="step5.php">ステップ 5: カートに追加</a></li>
-                <li><a href="step6.php">ステップ 6: カートを確認</a></li>
-                <li><a href="step7.php">ステップ 7: 注文を確定</a></li>
-            </ul>
-        </div>
+    <div class="flow_design09">
+    <ul class="flow09">
+      <li>
+        <p class="icon09">1</p>
+        <dl>
+          <dt>商品選択</dt>
+          <dd>商品をお選びください。</dd>
+        </dl>
+      </li>
+
+      <li>
+        <p class="icon09">2</p>
+        <dl>
+          <dt>枚数・プリント位置選択</dt>
+          <dd>ご注文のサイズと枚数、プリント加工をしたい位置を選んでください。</dd>
+        </dl>
+      </li>
+
+      <li>
+        <p class="icon09">3</p>
+        <dl>
+          <dt>商品購入</dt>
+          <dd>商品をカートに入れレジ画面へ進んでいただき手順に沿って購入を完了してください。</dd>
+        </dl>
+      </li>
+      <li>
+        <p class="icon09">4</p>
+        <dl>
+          <dt>デザイン送付</dt>
+          <dd>プリントしたいデザインを当社へご送付ください。デザインの作成にはデザインシートがございますのでそちらをお使いください。別紙でも対応いたしておりますのでその際は必要事項（担当名、団体名、連絡先）の記入をお願いいたします。送付方法は用紙を直接郵送いただくか公式LINEがございますので、そちらにPDF形式のファイルでお送りください。送付方法は商品購入の際にお選びください。</dd>
+          <dd>公式LINE</dd>
+        </dl>
+      </li>
+      <li>
+        <p class="icon09">5</p>
+        <dl>
+          <dt>デザイン確認</dt>
+          <dd>当社でお送りいただいたデザインをもとにプリント後のイメージを作成しお客様にお送りさせていただきますのでデザインの最終確認をお願いいたします。そのままでよろしい場合はそのままご返送ください。デザインの修整等ございましたら修整を記入頂きご返送ください。</dd>
+        </dl>
+      </li>
+      <li>
+        <p class="icon09">6</p>
+        <dl>
+          <dt>商品購入</dt>
+          <dd>商品をカートに入れレジ画面へ進んでいただき手順に沿って購入を完了してください。</dd>
+        </dl>
+      </li>
+      <li>
+        <p class="icon09">7</p>
+        <dl>
+          <dt>商品到着</dt>
+          <dd>お送り頂いたデザインでプリントさせていただきます。完成後お客様のもとへお送りさせていただきます。通常三日ほどでお送りいたしますので楽しみにお待ちください。</dd>
+        </dl>
+      </li>
+    </ul>
+  </div>
     </main>
     <footer>
         &copy; <?php echo date("Y"); ?> オリジナルTシャツ作成サイト
