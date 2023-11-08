2ea8ae93e7f958e44b9b4c9497354037439473a8
2201236 <2201236@s.asojuku.ac.jp>
2023-11-08 12:21:05 +0900

    new

---
+++ shoppingsite/home.php
@@ -0,0 +1,27 @@
+<?php require 'headerhome.php'; ?>
+<h1>カテゴリーから探す</h1>
+<div class="image-grid">
+    <div>
+        <img src="./image/white.png" alt="Tシャツ">
+        <p>Tシャツ</p>
+    </div>
+    <div>
+        <img src="./image/poro.png" alt="ポロシャツ">
+        <p>ポロシャツ</p>
+    </div>
+    <div>
+        <img src="./image/swe.png" alt="スウェット" style="heigth: 300px;">
+        <p>スウェット</p>
+    </div>
+    <div>
+        <img src="./image/paka.png" alt="パーカー">
+        <p>パーカー</p>
+    </div>
+    <div>
+        <img src="./image/top.png" alt="タンクトップ">
+        <p>タンクトップ</p>
+    </div>
+</div>
+<h1>ピックアップ商品</h1>
+
+<?php require 'footer.php'; ?>
