a22ee4b8a81fc496f18f24e4989cf5ae1b81a2bf
Naachaan <2201144@s.asojuku.ac.jp>
2023-11-09 09:57:56 +0900

    Create shohin-list.php

---
+++ shoppingsite/shohin-list.php
@@ -0,0 +1,25 @@
+<?php require 'header.php'; ?>
+
+<div class="sidecontent">
+  <!-- ここにコンテンツを追加 -->
+  <p><a href="">作成手順</a>について</p>
+  <p>SIZE</p>
+  <img src="./image/size.png" alt="サイズ表"width="600px" height="180px">
+  <p>PRINP</p>
+  <img src="./image/print.png" alt="プリント位置"width="600px" height="240px">
+  <p>
+    【発送までの目安】<br>
+    ご注文翌日より2週間程度でのお届けとなります。<br>
+    <br>
+    【送料】<br>
+    一律５００円とさせていただきます。<br>
+    <br>
+    【お支払い方法】<br>
+    銀行振込<br>
+    着払い<br>
+    <br>
+    【見積書】<br>
+    お見積書の対応はできません。ご注文を進めていただくと、カート内画面に、ご注文金額の記載がありますので、ご注文画面でご確認ください。<br>
+  </p>
+</div>
+<?php require 'footer.php'; ?>
