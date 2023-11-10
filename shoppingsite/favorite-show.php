e18831c6be637732e17f84e99f3eff028ccfdb6c
katuomirin <2003euph@gmail.com>
2023-11-10 13:34:44 +0900

    Update favorite-show.php

--- shoppingsite/favorite-show.php
+++ shoppingsite/favorite-show.php
@@ -1,6 +1,5 @@
-<?php require 'header.php'; ?>
 <?php session_start(); ?>
 
-<?php require 'menu.php'; ?>
+<?php require 'header.php'; ?>
+
 <?php require 'favorite.php'; ?>
-<?php require 'footer.php'; ?>
