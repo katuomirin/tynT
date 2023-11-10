076ff26e3e5d36a1e5f75dca785f943c5cf72609
katuomirin <2003euph@gmail.com>
2023-11-10 13:34:40 +0900

    Update favorite-incert.php

--- shoppingsite/favorite-incert.php
+++ shoppingsite/favorite-incert.php
@@ -3,9 +3,9 @@
 <?php require 'menu.php'; ?>
 <?php
 require 'db-connect.php';
-if(isset($_SESSION['customer'])){
+if(isset($_SESSION['user'])){
     $sql=$pdo->prepare('insert into favorite values(?,?)');
-    $sql->execute([$_SESSION['customer']['id'],$_GET['id']]);
+    $sql->execute([$_SESSION['user']['id'],$_GET['id']]);
 echo '<p>お気に入りに商品を追加しました。</p>';
 echo '<hr>';
 require 'favorite.php';
