5dda2375c08a4c6238c39d98f54a4a3629d2e20f
Naachaan <148741219+Naachaan@users.noreply.github.com>
2023-11-12 15:58:23 +0900

    Update shohin-list.php

--- shoppingsite/shohin-list.php
+++ shoppingsite/shohin-list.php
@@ -1,25 +1,33 @@
 <?php require 'header.php'; ?>
-
-<div class="sidecontent">
-  <!-- ここにコンテンツを追加 -->
-  <p><a href="">作成手順</a>について</p>
-  <p>SIZE</p>
-  <img src="./image/size.png" alt="サイズ表"width="600px" height="180px">
-  <p>PRINP</p>
-  <img src="./image/print.png" alt="プリント位置"width="600px" height="240px">
-  <p>
-    【発送までの目安】<br>
-    ご注文翌日より2週間程度でのお届けとなります。<br>
-    <br>
-    【送料】<br>
-    一律５００円とさせていただきます。<br>
-    <br>
-    【お支払い方法】<br>
-    銀行振込<br>
-    着払い<br>
-    <br>
-    【見積書】<br>
-    お見積書の対応はできません。ご注文を進めていただくと、カート内画面に、ご注文金額の記載がありますので、ご注文画面でご確認ください。<br>
-  </p>
+<link rel="stylesheet" href="./css/shohin-list.css">
+<body class="shohin">
+<div class="shohin-item">
+    <img src="./image/pink.png" width="200px">
 </div>
-<?php require 'footer.php'; ?>
+<div class="shohin-gaiyo">
+    <p><a href="">作成手順</a>について</p>
+    <p>SIZE</p>
+    <img src="./image/size.png" alt="サイズ表" height="180px">
+    <p>PRINP</p>
+    <img src="./image/print.png" alt="プリント位置" height="240px">
+    <p>
+        <p>発送までの目安</p>
+        ご注文翌日より2週間程度でのお届けとなります。<br>
+        <br>
+        <p>送料</p>
+        一律５００円とさせていただきます。<br>
+        <br>
+        <p>お支払方法</p>
+        クレジットカード<br>
+        コンビニ<br>
+        銀行振込<br>
+        着払い<br>
+        <br>
+        <p>見積書</p>
+        お見積書の対応はできません。ご注文を進めていただくと、カート内画面に、ご注文金額の記載がありますので、ご注文画面でご確認ください。<br>
+        </p>
+</div>
+</body>
+    <div class="footer">
+        <?php require 'footer.php'; ?>
+    </div>
