81aabd36ef716a4cdeedb0a9cdfe47dcc0241ba8
yamasita123 <2201209@s.asojuku.ac.jp>
2023-11-14 10:56:22 +0900

    Create reji_end1.php

---
+++ shoppingsite/reji_end1.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'; ?>
+<?php if($_SERVER["REQUEST_METHOD"] != "POST"){
+            header("Location: reji.php");
+            exit();
+        }
+?>
+    <center>
+        <body>
+            <h2>商品の購入が完了しました</h2>
+            <h2><a href="home.php">購入</a>を続ける</h2>
+
+            <h2>会員登録が完了しました</h2>
+        </body>
+    </center>
