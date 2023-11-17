e1d9f07a9d1c1a9dea95c528ce6d9e8bcea50d3d
katuomirin <2003euph@gmail.com>
2023-11-17 12:00:28 +0900

    Update favorite-incert.php

--- shoppingsite/favorite-incert.php
+++ shoppingsite/favorite-incert.php
@@ -3,14 +3,20 @@
 
 <?php
 require 'db-connect.php';
-if(isset($_SESSION['user'])){
-    $sql=$pdo->prepare('insert into favorite values(?,?)');
-    $sql->execute([$_SESSION['user']['id'],$_GET['id']]);
-echo '<p>お気に入りに商品を追加しました。</p>';
-echo '<hr>';
-require 'favorite.php';
-}else{
+
+if (isset($_SESSION['user'])) {
+    $sql = $pdo->prepare('INSERT INTO favorite (user_id, product_id) VALUES (?, ?)');
+    $sql->execute([$_SESSION['user']['id'], $_GET['id']]);
+
+    if ($sql->execute()) {
+        header('Location: favorite.php');
+        exit();
+    } else {
+        echo 'Error adding to favorites';
+    }
+} else {
     echo 'お気に入りに商品を追加するには、ログインしてください。';
 }
+
 ?>
 <?php require 'footer.php'; ?>
