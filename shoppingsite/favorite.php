909923c8330f94ae79caeb242a72b4aff051f5bb
katuomirin <2003euph@gmail.com>
2023-11-17 12:00:33 +0900

    Update favorite.php

--- shoppingsite/favorite.php
+++ shoppingsite/favorite.php
@@ -1,25 +1,29 @@
+<?php session_start(); ?>
 <?php require 'header.php'; ?>
 <?php
+require 'db-connect.php';
+
 if (isset($_SESSION['user'])) {
     echo '<table>';
     echo '<tr><th>商品番号</th><th>商品名</th><th>価格</th><th></th></tr>';
-$sql=$pdo->prepare('select * from favorite,product where user_id=? and product_id=id');
-$sql->execute([$_SESSION['user']['id']]);
-foreach($sql as $row){
-    $id=$row['id'];
-    echo '<tr>';
-    echo '<td>',$id,'</td>';
-    echo '<td><a href="detail.php?id=',$id,'">',$row['name'],'</a></td>';
-    echo '<td>',$row['price'],'</td>';
-    echo '<td><a href="favorite-delete.php?id=',$id,'">削除</a></td>';
-    echo '</tr>';
-}
+
+    $sql = $pdo->prepare('SELECT * FROM favorite INNER JOIN product ON favorite.product_id = product.id WHERE user_id = ?');
+    $sql->execute([$_SESSION['user']['id']]);
+
+    foreach ($sql as $row) {
+        $id = $row['id'];
+        echo '<tr>';
+        echo '<td>', $id, '</td>';
+        echo '<td><a href="detail.php?id=', $id, '">', htmlspecialchars($row['name']), '</a></td>';
+        echo '<td>', htmlspecialchars($row['price']), '</td>';
+        echo '<td><a href="favorite-delete.php?id=', $id, '">削除</a></td>';
+        echo '</tr>';
+    }
+
     echo '</table>';
 } else {
     echo 'お気に入りを表示するにはログインしてください。';
 }
+
+require 'footer.php';
 ?>
-
-
-
-<?php require 'footer.php'; ?>
