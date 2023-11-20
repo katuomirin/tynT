9a70e8664204e0123c75c50be42aed4f8d1303ef
yamasita123 <2201209@s.asojuku.ac.jp>
2023-11-20 13:44:47 +0900

    Update cart-incert.php

--- shoppingsite/cart-incert.php
+++ shoppingsite/cart-incert.php
@@ -1,10 +1,7 @@
 <?php session_start(); ?>
 <?php require 'header.php'; ?>
-<?php require 'db-connect.php';?>
 <?php
 if(isset($_SESSION['user'])){
-    $sql=$pdo->prepare('insert into cart values(?,?)');
-    $sql->execute([$_SESSION['user']['id'],$_GET['id']]);
 echo '<p>カートに商品を追加しました。</p>';
 echo '<hr>';
 require 'cart.php';
