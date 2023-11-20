85bf686d9536d29ba01f53d89c3a6783d65a47c7
katuomirin <2003euph@gmail.com>
2023-11-20 14:41:59 +0900

    Update db-connect.php

--- shoppingsite/db-connect.php
+++ shoppingsite/db-connect.php
@@ -1,9 +1,9 @@
 <?php
-     const SERVER = 'mysql218.phy.lolipop.lan';
-     const DBNAME = 'LAA1517465-sistem';
-     const USER ='LAA1517465';
-     const PASS ='Pass0124';
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-    $pdo=new PDO('mysql:host=mysql218.phy.lolipop.lan;dbname=LAA1517465-sistem;charset=utf8','LAA1517465','Pass0124');
+const SERVER = 'mysql218.phy.lolipop.lan';
+const DBNAME = 'LAA1517465-sistem';
+const USER = 'LAA1517465';
+const PASS = 'Pass0124';
 
+$connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+$pdo = new PDO($connect, USER, PASS);
 ?>
