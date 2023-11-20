5d413b81c25b80b871c049f07f7da4db6a2618dc
2201236 <2201236@s.asojuku.ac.jp>
2023-11-20 14:17:42 +0900

    h

--- shoppingsite/T-details.php
+++ shoppingsite/T-details.php
@@ -7,21 +7,20 @@
         $sql = $pdo->prepare('select * from product where id=?');
     $sql->execute([$_GET['id']]);
 
-<<<<<<< Updated upstream
     foreach ($sql as $row){
         echo '<p class="shohin-item"><img alt="image" style="width: 300px; margin:0; 
               display:block;" src="image/', $row['image'], '.png"></p>';//商品写真
         echo '<p class="shohin-shosai">';
-        echo '<h3>', $row['name'],'</h3>';
-        echo '<p>￥', $row['price'], '</p>';
+        echo '<p class="font1">', $row['name'],'</p>';
+        echo '<p class="font2">￥', $row['price'], '</p>';
         echo '<p>', $row['ex'], '</p>';
         echo '<p>素材:', $row['sozai'], '<br>';
         echo 'カラー:', $row['color'], '</p>';
 
-        echo '<p>加工位置を選択してください<br>';
-        echo '※加工費は一律５００円です。</p>';
-        echo '<form method="post" action="">
-                <label><input type="checkbox" name="options[]" value="左胸（10×10㎝）"> 左胸（10×10㎝）</label><br>
+        echo '<p class="font3">加工位置を選択してください</p>';
+        echo '<p class="font4">※加工費は一律５００円です。</p>';
+        echo '<form method="post" action="cart-incert.php" class="position">
+                <label><input type="checkbox" name="options[]" value="左胸（10×10㎝）"></label><br>
                 <label><input type="checkbox" name="options[]" value="右胸(10×10cm）"> 右胸(10×10cm）</label><br>
                 <label><input type="checkbox" name="options[]" value="胸中央(35.5×40cm）"> 胸中央(35.5×40cm）</label><br>
                 <label><input type="checkbox" name="options[]" value="背中中央（35.5×40cm）"> 背中中央（35.5×40cm）</label><br>
@@ -48,9 +47,9 @@
     ?>
     <div class="shohin-gaiyo">
         <p><a href="create.php">作成手順</a>について</p>
-        <p>SIZE</p>
+        <p class="saize">SIZE</p>
         <img src="./image/size.png" alt="サイズ表" width="100%">
-        <p>PRINP</p>
+        <p class="print">PRINT</p>
         <img src="./image/print.png" alt="プリント位置" width="100%">
         <p>
             <p>発送までの目安</p>
@@ -72,29 +71,4 @@
 </body>
 <div class="footer">
     <?php require 'footer.php'; ?>
-=======
-<div class="sidecontent">
-  <!-- ここにコンテンツを追加 -->
-  <p><a href="">作成手順</a>について</p>
-  <p>SIZE</p>
-  <img src="./image/size.png" alt="サイズ表"width="600px" height="180px">
-  <p>PRINP</p>
-  <img src="./image/print.png" alt="プリント位置"width="600px" height="240px">
-  <p>
-    【発送までの目安】<br>
-    ご注文翌日より2週間程度でのお届けとなります。<br>
-    <br>
-    【送料】<br>
-    一律５００円とさせていただきます。<br>
-    <br>
-    【お支払い方法】<br>
-    クレジットカード<br>
-    コンビニ<br>
-    銀行振込<br>
-    着払い<br>
-    <br>
-    【見積書】<br>
-    お見積書の対応はできません。ご注文を進めていただくと、カート内画面に、ご注文金額の記載がありますので、ご注文画面でご確認ください。<br>
-  </p>
->>>>>>> Stashed changes
 </div>
