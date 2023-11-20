2cab2fb09a4f77c70b26e1b991c92f65c86668d8
Naachaan <2201144@s.asojuku.ac.jp>
2023-11-20 14:06:49 +0900

    Update shohin-list.php

--- shoppingsite/shohin-list.php
+++ shoppingsite/shohin-list.php
@@ -7,6 +7,7 @@
         $sql = $pdo->prepare('select * from product where id=?');
     $sql->execute([$_GET['id']]);
 
+<<<<<<< Updated upstream
     foreach ($sql as $row){
         echo '<p class="shohin-item"><img alt="image" style="width: 300px; margin:0; 
               display:block;" src="image/', $row['image'], '.png"></p>';//商品写真
@@ -71,4 +72,29 @@
 </body>
 <div class="footer">
     <?php require 'footer.php'; ?>
+=======
+<div class="sidecontent">
+  <!-- ここにコンテンツを追加 -->
+  <p><a href="">作成手順</a>について</p>
+  <p>SIZE</p>
+  <img src="./image/size.png" alt="サイズ表"width="600px" height="180px">
+  <p>PRINP</p>
+  <img src="./image/print.png" alt="プリント位置"width="600px" height="240px">
+  <p>
+    【発送までの目安】<br>
+    ご注文翌日より2週間程度でのお届けとなります。<br>
+    <br>
+    【送料】<br>
+    一律５００円とさせていただきます。<br>
+    <br>
+    【お支払い方法】<br>
+    クレジットカード<br>
+    コンビニ<br>
+    銀行振込<br>
+    着払い<br>
+    <br>
+    【見積書】<br>
+    お見積書の対応はできません。ご注文を進めていただくと、カート内画面に、ご注文金額の記載がありますので、ご注文画面でご確認ください。<br>
+  </p>
+>>>>>>> Stashed changes
 </div>
