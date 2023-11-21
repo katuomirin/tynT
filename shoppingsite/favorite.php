25c81363a5ffaeab9583e78807cb803da8fad5bb
katuomirin <2003euph@gmail.com>
2023-11-21 10:52:31 +0900

    Update favorite.php

--- shoppingsite/favorite.php
+++ shoppingsite/favorite.php
@@ -4,7 +4,7 @@ require 'db-connect.php';
 
 if (isset($_SESSION['user'])) {
     echo '<table>';
-    echo '<tr><th>商品番号</th><th>商品名</th><th>価格</th><th></th></tr>';
+    echo '<tr><th>商品番号</th><th>商品名</th><th>価格</th><th>画像</th><th></th></tr>';
 
     $sql = $pdo->prepare('SELECT * FROM favorite INNER JOIN product ON favorite.product_id = product.id WHERE user_id = ?');
     $sql->execute([$_SESSION['user']['id']]);
@@ -15,6 +15,7 @@ if (isset($_SESSION['user'])) {
         echo '<td>', $id, '</td>';
         echo '<td><a href="detail.php?id=', $id, '">', htmlspecialchars($row['name']), '</a></td>';
         echo '<td>', htmlspecialchars($row['price']), '</td>';
+        echo '<td><img src="path_to_your_images_directory/', htmlspecialchars($row['image']), '" alt="Product Image" style="width: 100px; height: 100px;"></td>';
         echo '<td><a href="favorite-delete.php?id=', $id, '">削除</a></td>';
         echo '</tr>';
     }
