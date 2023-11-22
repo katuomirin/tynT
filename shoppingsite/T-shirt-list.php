510a88af1a486e571a29fdebf350774566f11da8
Naachaan <2201144@s.asojuku.ac.jp>
2023-11-22 14:55:49 +0900

    n

--- shoppingsite/T-shirt-list.php
+++ shoppingsite/T-shirt-list.php
@@ -18,7 +18,7 @@
     
         foreach ($sql as $row) {
             $id = $row['id'];
-            echo '<img alt="image" style="width: 40px; height: 40px;" src="image/',$row['image'], '.png"><br>';
+            echo '<img alt="image" src="image/',$row['image'], '.png"><br>';
             echo '<a href="T-details.php?id=', $id, '">', $row['name'], '</a><br>';
             echo '<td>', $row['price'], '</td><br>';
         }
