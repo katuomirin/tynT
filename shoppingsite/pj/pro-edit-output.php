b8996f81e2db07ba2f91684fe7a10415bde94dfd
Kino1412 <nhoccan1412@gmail.com>
2023-11-22 12:46:46 +0900

    update

--- shoppingsite/pj/pro-edit-output.php
+++ shoppingsite/pj/pro-edit-output.php
@@ -13,11 +13,23 @@ $pdo=new PDO($connect, USER, PASS);
     if (empty($_POST['name'])) {
         echo '商品名を入力してください。';
     } else
-    if (empty($_POST[''])) {
+    if (empty($_POST['ex'])) {
         echo '商品説明を入力してください。';
     } else
     if (!preg_match('/[0-9]+/', $_POST['price'])) {
         echo '商品価格を整数で入力してください。';
+    } else
+    if (empty($_POST['size'])) {
+        echo '商品サイズを入力してください。';
+    } else
+    if (empty($_POST['color'])) {
+        echo '商品色を入力してください。';
+    } else
+    if (empty($_POST['sozai'])) {
+        echo '商品素材を入力してください。';
+    } else
+    if (!preg_match('/[0-9]+/', $_POST['zaiko'])) {
+        echo '商品在庫を整数で入力してください。';
     } else
     if($sql->execute([htmlspecialchars($_POST['name']),$_POST['ex'], $_POST['price'],$_POST['size'],$_POST['color'],$_POST['sozai'],$_POST['image'], $_POST['id']])){
         echo '<font color="red">更新に成功しました。</font>';
@@ -29,14 +41,15 @@ $pdo=new PDO($connect, USER, PASS);
         <hr>
 <?php
 foreach ($pdo->query('select * from product') as $row) {
-    echo '<tr>';
-    echo '<td>', $row['id'], '</td>';
-    echo '<td>', $row['name'], '</td>';
-    echo '<td>', $row['ex'], '</td>';
-    echo '<td>', $row['price'], '</td>';
-    echo '<td>', $row['image'], '</td>';
-    echo '<td>', $row['koshinbi'], '</td>';
-    echo '</tr>';
+        echo '<p>商品番号：' . $row['id'] . '</p>';
+        echo '<p>商品名：' . $row['name'] . '</p>';
+        echo '<p>商品名：' . $row['ex'] . '</p>';
+        echo '<p>価格：' . $row['price'] . '</p>';
+        echo '<p>サイズ：' . $row['size'] . '</p>';
+        echo '<p>色：' . $row['color'] . '</p>';
+        echo '<p>素材：' . $row['sozai'] . '</p>';
+        echo '<p>在庫：' . $row['zaiko'] . '</p>';
+        echo '<img src="' . $row['image'] . '" alt="Product Image" width="200" height="150">';
     echo "\n";
 }
 ?>
