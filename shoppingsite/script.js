d1cad94eba1542b04aa905436fee40791d5a3af3
katuomirin <2003euph@gmail.com>
2023-11-24 14:43:45 +0900

    Update script.js

--- shoppingsite/script.js
+++ shoppingsite/script.js
@@ -9,3 +9,26 @@ btn.onclick = () => {
 mask.onclick = () => {
   nav.classList.toggle("open");
 };
+
+function calculateSubtotal(subtotalId, quantityInputId, priceId) {
+  var quantity = parseInt(document.getElementById(quantityInputId).value) || 0;
+  var price = parseFloat(document.getElementById(priceId).innerText.replace('￥', '')) || 0;
+
+  if (!isNaN(quantity) && !isNaN(price)) {
+      var subtotal = quantity * price;
+      document.getElementById(subtotalId).textContent = subtotal;
+  }
+  calculateTotalQuantity();  // 合計数量も再計算
+}
+
+function calculateTotalQuantity() {
+  var quantityS = parseInt(document.getElementById('quantityInputS').value) || 0;
+  var quantityM = parseInt(document.getElementById('quantityInputM').value) || 0;
+  var quantityL = parseInt(document.getElementById('quantityInputL').value) || 0;
+  var quantityXL = parseInt(document.getElementById('quantityInputXL').value) || 0;
+  var quantityXXL = parseInt(document.getElementById('quantityInputXXL').value) || 0;
+
+  var totalQuantity = quantityS + quantityM + quantityL + quantityXL + quantityXXL;
+
+  document.getElementById('totalQuantity').innerText = totalQuantity;
+}
