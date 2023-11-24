e6c02549c2b12076b86e0e5082cb20fd639f7127
katuomirin <2003euph@gmail.com>
2023-11-24 14:43:42 +0900

    Update favorite-incert.php

--- shoppingsite/favorite-incert.php
+++ shoppingsite/favorite-incert.php
@@ -1,27 +1,32 @@
 <?php session_start(); ?>
 <?php require 'header.php'; ?>
-<?php
-require 'db-connect.php';
+<?php require 'db-connect.php'; ?>
 
-if (isset($_SESSION['user'])) {
-    $user_id = $_SESSION['user']['id'];
+<?php
+if (isset($_GET['id'])) {
     $product_id = $_GET['id'];
 
-    // Check if the record already exists
-    $checkSql = $pdo->prepare('SELECT * FROM favorite WHERE user_id = ? AND product_id = ?');
-    $checkSql->execute([$user_id, $product_id]);
+    if (isset($_SESSION['user'])) {
+        $user_id = $_SESSION['user']['id'];
 
-    if ($checkSql->rowCount() > 0) {
-        // Product is already a favorite
-        echo 'この商品はすでにお気に入りに登録されています。';
+        // Check if the record already exists
+        $checkSql = $pdo->prepare('SELECT * FROM favorite WHERE user_id = ? AND product_id = ?');
+        $checkSql->execute([$user_id, $product_id]);
+
+        if ($checkSql->rowCount() > 0) {
+            // Product is already a favorite
+            echo 'この商品はすでにお気に入りに登録されています。';
+        } else {
+            // Insert the new favorite
+            $insertSql = $pdo->prepare('INSERT INTO favorite (user_id, product_id) VALUES (?, ?)');
+            $insertSql->execute([$user_id, $product_id]);
+            echo 'お気に入りに商品を追加しました。';
+        }
     } else {
-        // Insert the new favorite
-        $insertSql = $pdo->prepare('INSERT INTO favorite (user_id, product_id) VALUES (?, ?)');
-        $insertSql->execute([$user_id, $product_id]);
-        echo 'お気に入りに商品を追加しました。';
+        echo 'お気に入りに商品を追加するには、ログインしてください。';
     }
 } else {
-    echo 'お気に入りに商品を追加するには、ログインしてください。';
+    echo '商品IDが指定されていません。';
 }
 
 require 'footer.php';
