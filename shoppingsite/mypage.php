b7ad48718bddc93c0b638e0ae5138e138bc0ebec
Naachaan <2201144@s.asojuku.ac.jp>
2023-11-24 13:46:54 +0900

    n

--- shoppingsite/mypage.php
+++ shoppingsite/mypage.php
@@ -1,6 +1,23 @@
-<?php require 'header-user.php'; ?>
-
-
-
-
+<?php require 'header.php'; ?>
+<?php require 'db-connect.php'; ?>
+<link rel="stylesheet" href="./css/mypage.css">
+<body>
+<?php
+    $pdo = new PDO($connect, USER, PASS);
+        $sql = $pdo->prepare('select * from user where id=?');
+    $sql->execute([$_GET['id']]);
+    $product = new PDO($connect, USER, PASS);
+        $shohin = $product->prepare('select * from product where id=?');
+    $shohin->execute([$_GET['id']]);
+    foreach ($sql as $row){
+        echo '<h1>Hi,',$row['kanji'],'</h1>';
+        foreach($shohin as $roow){
+            echo '<p>カート</p>';
+            
+            echo '<P>お気に入り</p>';
+            echo '<P>注文履歴</p>';
+        }
+    }
+?>
+</body>
 <?php require 'footer.php'; ?>
