4487713e0f3f3eb0640fa945658b67ee706aad76
Naachaan <2201144@s.asojuku.ac.jp>
2023-11-26 00:09:45 +0900

    n

--- shoppingsite/T-shirt-list.php
+++ shoppingsite/T-shirt-list.php
@@ -14,16 +14,19 @@
         $sql = $pdo->prepare('select * from product where category = :category');
         $sql->bindParam(':category', $T, PDO::PARAM_STR);
         $sql->execute();
-        
+
+        echo '<div class="item">';
             foreach ($sql as $row) {
                 $id = $row['id'];
+                echo '<div class="shohins">';
                 echo '<img class="img" alt="image" src="image/',$row['image'], '.png"><br>';
                 echo '<a href="T-details.php?id=', $id, '">', $row['name'], '</a>';
                 echo '<div class="example2">
                         <input type="checkbox" checked id="1" name="example2"><label for="1"></label>
-                      </div><br>';
-                echo '<td>', $row['price'], '</td><br>';
+                      </div>';
+                echo '<p class="price">', $row['price'], '</p></div>';
             }
+        echo '</div>'
         ?>
 </body>
     <div class="footer">
