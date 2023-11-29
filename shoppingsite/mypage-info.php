ada87dbe4f55cf67ec4b74c347e994711eb61392
katuomirin <2003euph@gmail.com>
2023-11-29 14:21:11 +0900

    Update mypage-info.php

--- shoppingsite/mypage-info.php
+++ shoppingsite/mypage-info.php
@@ -7,22 +7,18 @@
     $kana = $kanji = $email = $password = $birthday = $gender = $post_code
     = $prefectures = $address1 = $address2 = $manshon = '';
     $pdo = new PDO($connect, USER, PASS);
-
     if(isset($_SESSION['user'])){
-        $user = $_SESSION['user'];
-
-        // Assign values to variables
-        $kana = $user['kana'];
-        $kanji = $user['kanji'];
-        $email = $user['email'];
-        $password = $user['password'];
-        $birthday = $user['birthday'];
-        $gender = $user['gender'];
-        $post_code = $user['post_code'];
-        $prefectures = $user['prefectures'];
-        $address1 = $user['address1'];
-        $address2 = $user['address2'];
-        $manshon = $user['manshon'];
+        $kana = $_SESSION['user']['kana'];
+        $kanji = $_SESSION['user']['kanji'];
+        $email = $_SESSION['user']['email'];
+        $password = $_SESSION['user']['password'];
+        $birthday = $_SESSION['user']['birthday'];
+        $gender = $_SESSION['user']['gender'];
+        $post_code = $_SESSION['user']['post_code'];
+        $prefectures = $_SESSION['user']['prefectures'];
+        $address1 = $_SESSION['user']['address1'];
+        $address2 = $_SESSION['user']['address2'];
+        $manshon = $_SESSION['user']['manshon'];
 
         echo '<p>カナ:', $kana, '</p>';
         echo '<p>漢字:', $kanji, '</p>'; 
@@ -32,12 +28,21 @@
         echo '<p>性別:', $gender, '</p>';
         echo '<p>郵便番号:', $post_code, '</p>';
         echo '<p>都道府県:', $prefectures, '</p>';
-        echo '<p>住所1:', $address1, '</p>';
-        echo '<p>住所2:', $address2, '</p>';
-        echo '<p>マンション:', $manshon, '</p>';
+        echo '<p>', $address1, '</p>';
+        echo '<p>', $address2, '</p>';
+        echo '<p>', $manshon, '</p>';
+        
+        // Add a form to handle changes
+        echo '<form action="mypage-change.php" method="post">';
+        echo '<button type="submit">変更</button>';
+        echo '</form>';
     } else {
-        echo '<p>ユーザー情報が見つかりません。</p>';
+        echo '<p>ログイン情報が見つかりません。<br>もう一度ログインしてください</p>';
+        echo '<a href="login.php">ログイン</a>';
     }
+    echo '<form action="mypage-change.php" method="post">';
+    echo '<button>変更</button>';
+    echo '</form>';
 ?>
 </body>
 <?php require 'footer.php'; ?>
