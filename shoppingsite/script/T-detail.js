8d72525183af84c3eebd48730c957808e6858a18
katuomirin <2003euph@gmail.com>
2023-11-30 01:14:50 +0900

    Update T-detail.js

--- shoppingsite/script/T-detail.js
+++ shoppingsite/script/T-detail.js
@@ -11,26 +11,26 @@ function calculateSubtotal(subtotalId, quantityInputId, price) {
 }
 
 function calculateTotalQuantity() {
-    var quantityS = parseInt(document.getElementById('quantityInputS').value) || 0;
-    var quantityM = parseInt(document.getElementById('quantityInputM').value) || 0;
-    var quantityL = parseInt(document.getElementById('quantityInputL').value) || 0;
-    var quantityXL = parseInt(document.getElementById('quantityInputXL').value) || 0;
-    var quantityXXL = parseInt(document.getElementById('quantityInputXXL').value) || 0;
+    var sizes = ['S', 'M', 'L', 'XL', 'XXL'];
+    var totalQuantity = 0;
 
-    var totalQuantity = quantityS + quantityM + quantityL + quantityXL + quantityXXL;
+    sizes.forEach(function (size) {
+        var quantity = parseInt(document.getElementById('quantityInput' + size).value) || 0;
+        totalQuantity += quantity;
+    });
 
     document.getElementById('totalQuantity').innerText = totalQuantity;
 }
 
 function calculateTotalSubtotal() {
-    var subtotalS = parseInt(document.getElementById('subtotalS').textContent) || 0;
-    var subtotalM = parseInt(document.getElementById('subtotalM').textContent) || 0;
-    var subtotalL = parseInt(document.getElementById('subtotalL').textContent) || 0;
-    var subtotalXL = parseInt(document.getElementById('subtotalXL').textContent) || 0;
-    var subtotalXXL = parseInt(document.getElementById('subtotalXXL').textContent) || 0;
+    var totalSubtotal = 0;
 
-    var totalSubtotal = subtotalS + subtotalM + subtotalL + subtotalXL + subtotalXXL;
-    alert("aa");
+    // 各サイズの小計を取得
+    var sizes = ['S', 'M', 'L', 'XL', 'XXL'];
+    sizes.forEach(function (size) {
+        totalSubtotal += parseInt(document.getElementById('subtotal' + size).textContent) || 0;
+    });
+
     // 加工位置のチェックボックスを取得
     var checkboxes = document.querySelectorAll('form.check input[type="checkbox"]');
     checkboxes.forEach(function (checkbox) {
@@ -38,24 +38,24 @@ function calculateTotalSubtotal() {
             totalSubtotal += 500 * parseInt(checkbox.getAttribute('data-quantity')) || 0;
         }
     });
-    var work = document.getElementsByName("options");
-    var tmp=0;
-    alert("a");
-    for(var i=0; i<work.length; i++){
-        alert("b");
-        if (work[i].checked) {
-            tmp += 500 ;
-            alert(work[i].value);
+
+    // optionsの取得
+    var options = document.getElementsByName("options");
+    options.forEach(function (option) {
+        if (option.checked) {
+            totalSubtotal += 500;
+            alert(option.value);
         }
-    }
-    document.getElementById('processingFee').innerText = tmp;
+    });
+
+    document.getElementById('processingFee').innerText = totalSubtotal;
 
     document.getElementById('totalSubtotal').innerText = totalSubtotal;
 }
 
 // お気に入りのチェックボックスに対する jQuery コード
-$(document).ready(function() {
-    $(".checkbox").click(function() {
+$(document).ready(function () {
+    $(".checkbox").click(function () {
         $(this).toggleClass("is-checked");
     });
 });
