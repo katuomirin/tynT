b3467e833c84b987860b111885fc3b8a63c04b60
yamasita123 <2201209@s.asojuku.ac.jp>
2023-11-29 14:35:52 +0900

    Update T-details.php

--- shoppingsite/T-details.php
+++ shoppingsite/T-details.php
@@ -109,6 +109,7 @@
         echo '<input type="hidden" name="name" value="', $row['name'], '">';
         echo '<input type="hidden" name="price" value="', $row['price'], '">';
         echo '<input type="hidden" name="subtotal" id="subtotalInput" value="0">'; // 各商品の小計を保存するフィールド
+        echo '<input type="hidden" name="quantityOutput" id="quantityOutput" value="0">'; // 各商品の小計を保存するフィールド
         echo '<p class="cart-botton"><input type="submit" value="カートに追加" onclick="setHiddenFields();"></p>'; // カートに追加ボタン
 
         // お気に入りに追加
