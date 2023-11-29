64428e693953c731791244366300d8a08049fb18
Naachaan <2201144@s.asojuku.ac.jp>
2023-11-29 15:02:47 +0900

    n

--- shoppingsite/mypage.php
+++ shoppingsite/mypage.php
@@ -9,8 +9,8 @@
     if(isset($_SESSION['user'])){
         $user = $_SESSION['user'];
         echo '<h1>Hi,',$user['kanji'],'</h1>';
-        echo '<p>カート</p>';
-        echo '<div class="shohins">';
+        echo '<p class="cart">カート</p>';
+        echo '<div class="carts">';
         if(isset($_SESSION['cart_data']) && !empty($_SESSION['cart_data'])){
             foreach($_SESSION['cart_data'] as $cart){
                 echo '<div>';
@@ -22,16 +22,17 @@
             echo 'カートに商品が入っていません。';
         }
         echo "</div>";
-        echo '<p>お気に入り</p>';
-
+        echo '<p class="favorite">お気に入り</p>';
+        echo '<div class="favorites">';
+        
+        echo "</div>";
 
         echo '<a href="mypage-info.php" class="mypage-info-button">マイページ情報</a>';
-        echo '<a href="logout.php" >ログアウト</a>';
+        echo '<p><a href="logout.php" >ログアウト</a></p>';
     }else{
         echo '<p>ログイン情報が見つかりません。<br>もう一度ログインしてください</p>';
         echo '<a href="login.php">ログイン</a>';
     }
-    echo '<button class="button-003">変更</button>';
     echo '</form>';
 ?>
 </body>
