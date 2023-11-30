50c129dc3c4300eb59b23fb384f9aa5d92b0768b
yamasita123 <2201209@s.asojuku.ac.jp>
2023-11-30 10:50:52 +0900

    Update cart-incert.php

--- shoppingsite/cart-incert.php
+++ shoppingsite/cart-incert.php
@@ -1,25 +1,31 @@
 <?php session_start(); ?>
 <?php require 'header.php'; ?>
+
+<?php
+$quantityOutput = $_GET['quantityOutput'];
+echo $quantityOutput;
+?>
+
 <?php
 $id=$_POST['id'];
 if (!isset($_SESSION['product'])) {
     $_SESSION['product']=[];
 }
-$count=0;
 if (isset($_SESSION['product'][$id])){
-    $count=$_SESSION['product'][$id]['totalQuantity'];
+    $quantityOutput=$_SESSION['product'][$id]['quantityOutput'];
 }
 $_SESSION['product'][$id]=[
     'name' => $_POST['name'],
     'price' => $_POST['price'],
-    'totalQuantity' => $_POST['totalQuantity']
+    'quantityOutput' => $_POST['quantityOutput'],
+    'totalSubtotal' => $_POST['totalSubtotal']
 ];
 echo '<form method="post" action="mypage.php">';//マイページでつかうよ
     $_SESSION['cart_data'][$id] = [
         'id' => $id,
         'name' => $_POST['name'],
         'price' => $_POST['price'],
-        'totalQuantity' => $_POST['totalQuantity']
+        'quantityOutput' => $_POST['quantityOutput']
     ];
 echo '</form>';
 echo '<p>カートに商品を追加しました。</p>';
