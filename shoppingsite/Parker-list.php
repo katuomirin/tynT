2784a6138a22e63ee081e1454bf550b4741c6190
Naachaan <2201144@s.asojuku.ac.jp>
2023-11-30 10:04:05 +0900

    n

--- shoppingsite/Parker-list.php
+++ shoppingsite/Parker-list.php
@@ -16,47 +16,52 @@
     $sql->execute();
     
     echo '<div class="item">';
-    foreach ($sql as $row) {
-        $id = $row['id'];
-        echo '<div class="shohins">';
-        echo '<a href="T-details.php?id=', $id, '"><img class="img" alt="image" src="image/',$row['image'], '.png"></a>';
-        echo '<div class="choice-list">
-                <div class="checkbox heart" onclick="favorite(',$id,)"></div>
-              </div>';
-        echo '<script></script>'
-        echo '<a href="T-details.php?id=', $id, '">', $row['name'], '</a>';
-        echo '<p class="price">', $row['price'], '</p></div>';
-    }
-    
-    echo   '<script>
-        function favorite(id){
-                $(".checkbox").click(function() {
-                    if (!$(this).hasClass("is-checked")) {
-                        console.log("クリック前の処理");
-                    }
-                    $(this).toggleClass("is-checked");
-                    if ($(this).hasClass("is-checked")) {
-                        console.log("クリック後の処理");
-                        var productId = ' + id + '; // 商品IDを取得
-                        $.ajax({
-                            type: "POST",
-                            url: "favorite-incert.php",
-                            data: {id: productId},
-                            success: function(response) {
-                                // レスポンスを処理する（必要に応じて）
-                                console.log(response);
-                            },
-                            error: function(error) {
-                                console.error(error);
+            foreach ($sql as $row) {
+                $id = $row['id'];
+                echo '<div class="shohins">';
+                echo '<a href="T-details.php?id=', $id, '"><img class="img" alt="image" src="image/',$row['image'], '.png"></a>';
+                echo '<div class="choice-list" data-postid="', $id, '">
+                        <div class="checkbox heart"></div>
+                    </div>';
+                echo '<a href="T-details.php?id=', $id, '">', $row['name'], '</a>';
+                echo '<p class="price">', $row['price'], '</p></div>';
+            }
+            echo   '<script>
+                    $(function() {
+                        var $favorite = $(\'.checkbox\'), //お気に入りボタンセレクタ
+                        productId;
+                        $favorite.on(\'click\',function(e){
+                            //カスタム属性（postid）に格納された投稿ID取得
+                            productId = $(this).parents(\'.choice-list\').data(\'postid\'); 
+                            console.log("クリック前の処理");
+                            console.log("ID=" + productId);
+                            if (!$(this).hasClass("is-checked")) {
+                                console.log("クリック前の処理");
+                            }
+                            $(this).toggleClass("is-checked");
+                            if ($(this).hasClass("is-checked")) {
+                                console.log("クリック後の処理");
+                                $.ajax({
+                                    type: "POST",
+                                    url: "favorite-incert.php",
+                                    data: {id: productId},
+                                    success: function(response) {
+                                        // レスポンスを処理する（必要に応じて）
+                                        console.log(response);
+                                    },
+                                    error: function(error) {
+                                        console.error(error);
+                                    }
+                                });
                             }
                         });
-                    }
-                });
-            }
-            </script>';
-    echo '</div>';
-?>
-</body>
-<div class="footer">
-<?php require 'footer.php'; ?>
+                    });
+                </script>';
+            echo '</div>';
+        ?>
 </div>
+
+        <div class="footer">
+        <?php require 'footer.php'; ?>
+    </div> 
+    </body>
