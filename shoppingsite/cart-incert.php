6d5f2dd825e2cfd1f6142de118c1f60ba037c4ce
yamasita123 <2201209@s.asojuku.ac.jp>
2023-11-30 11:04:22 +0900

    Update cart-incert.php

--- shoppingsite/cart-incert.php
+++ shoppingsite/cart-incert.php
@@ -2,22 +2,17 @@
 <?php require 'header.php'; ?>
 
 <?php
-$quantityOutput = $_GET['quantityOutput'];
-echo $quantityOutput;
-?>
-
-<?php
 $id=$_POST['id'];
 if (!isset($_SESSION['product'])) {
     $_SESSION['product']=[];
 }
 if (isset($_SESSION['product'][$id])){
-    $quantityOutput=$_SESSION['product'][$id]['quantityOutput'];
+    $quantityOutput=$_SESSION['product'][$id]['quantity'];
 }
 $_SESSION['product'][$id]=[
     'name' => $_POST['name'],
     'price' => $_POST['price'],
-    'quantityOutput' => $_POST['quantityOutput'],
+    'quantity' => $_POST['quantity'],
     'totalSubtotal' => $_POST['totalSubtotal']
 ];
 echo '<form method="post" action="mypage.php">';//マイページでつかうよ
@@ -25,7 +20,7 @@ echo '<form method="post" action="mypage.php">';//マイページでつかうよ
         'id' => $id,
         'name' => $_POST['name'],
         'price' => $_POST['price'],
-        'quantityOutput' => $_POST['quantityOutput']
+        'quantity' => $_POST['quantity']
     ];
 echo '</form>';
 echo '<p>カートに商品を追加しました。</p>';
