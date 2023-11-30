9d2a4cc2327d6b28abc9500f626a0394fcdcf11a
Naachaan <2201144@s.asojuku.ac.jp>
2023-12-01 00:43:10 +0900

    n

--- shoppingsite/favorite.php
+++ shoppingsite/favorite.php
@@ -1,48 +1,54 @@
 <?php require 'header.php'; ?>
 <?php require 'db-connect.php';?>
 <link rel="stylesheet" href="./css/favorite.css">
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <?php
     echo '<h1>お気に入り商品一覧</h1>';
     if(isset($_SESSION['user'])){
     $sql = $pdo->prepare('SELECT * FROM favorite INNER JOIN product ON favorite.product_id = product.id WHERE user_id = ?');
     $sql->execute([$_SESSION['user']['id']]);
     echo '<div class="item">';
-        foreach ($sql as $row) {
-            $id = $row['id'];
-            echo '<div class="shohins">';
-            echo '<a href="T-details.php?id=', $id, '"><img class="img" alt="image" src="image/',$row['image'], '.png"></a><br>';
-            echo '<a href="T-details.php?id=', $id, '">', $row['name'], '</a>';
-            echo '<p class="price">', $row['price'], '</p></div>';
-        }
-        }else{
-            echo 'お気に入りを表示するにはログインしてください。';
-        }
-/*body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-        .product {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin-bottom: 10px;
-            display: flex;
-            align-items: center; /* 垂直方向に中央揃え */
-        /*}
-        .product img {
-            max-width: 70px;
-            height: auto;
-            margin-right: 10px; /* 画像とテキストの間隔を設定 */
-        /*}
-        .product-info {
-            flex-grow: 1; /* テキスト部分を伸ばして残りのスペースを埋める */
-        /*}
-        button {
-            margin-top: 10px;
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-        */
+            foreach ($sql as $row) {
+                $id = $row['id'];
+                echo '<div class="shohins">';
+                echo '<a href="T-details.php?id=', $id, '"><img class="img" alt="image" src="image/',$row['image'], '.png"></a>';
+                echo '<div class="choice-list" data-postid="', $id, '">
+                        <div class="checkbox heart"></div>
+                    </div>';
+                echo '<a href="T-details.php?id=', $id, '">', $row['name'], '</a>';
+                echo '<p class="price">', $row['price'], '</p></div>';
+            }
+            echo   '<script>
+                    $(function() {
+                        var $favorite = $(\'.checkbox\'), //お気に入りボタンセレクタ
+                        productId;
+                        $favorite.on(\'click\',function(e){
+                            //カスタム属性（postid）に格納された投稿ID取得
+                            productId = $(this).parents(\'.choice-list\').data(\'postid\'); 
+                            console.log("クリック前の処理");
+                            console.log("ID=" + productId);
+                            if (!$(this).hasClass("is-checked")) {
+                                console.log("クリック前の処理");
+                            }
+                            $(this).toggleClass("is-checked");
+                            if ($(this).hasClass("is-checked")) {
+                                console.log("クリック後の処理");
+                                $.ajax({
+                                    type: "POST",
+                                    url: "favorite-delete.php",
+                                    data: {id: productId},
+                                    success: function(response) {
+                                        // レスポンスを処理する（必要に応じて）
+                                        console.log(response);
+                                    },
+                                    error: function(error) {
+                                        console.error(error);
+                                    }
+                                });
+                            }
+                        });
+                    });
+                </script>';
+            echo '</div>';
+    }
+            ?>
