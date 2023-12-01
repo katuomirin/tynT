35c323d2d7dfdcdfbe3eb017338879f90aa416a7
2201236 <2201236@s.asojuku.ac.jp>
2023-12-01 12:34:44 +0900

    Update home.php

--- shoppingsite/home.php
+++ shoppingsite/home.php
@@ -2,27 +2,31 @@
 <h1>カテゴリーから探す</h1>
 <div class="image-grid">
     <div>
-        <a href="T-shirt-list.php"> <img src="./image/white.png" alt="Tシャツ" ></a>
+        <a href="T-shirt-list.php"> <img src="./image/white.png" alt="Tシャツ" style="height: 300px; width: 300px;" ></a>
         <p>Tシャツ</p>
     </div>
     <div>
-    <a href="Polo-shirt-list.php"><img src="./image/poro.png" alt="ポロシャツ"></a>
+    <a href="Polo-shirt-list.php"><img src="./image/poro.png" alt="ポロシャツ" style="height: 300px; width: 300px;"></a>
         <p>ポロシャツ</p>
     </div>
     <div>
-    <a href="Sweat-list.php"><img src="./image/swe.png" alt="スウェット" style="heigth: 300px;"></a>
+    <a href="Sweat-list.php"><img src="./image/swe.png" alt="スウェット" style="height: 300px;width: 300px;"></a>
         <p>スウェット</p>
     </div>
     <div>
-    <a href="Parker-list.php"><img src="./image/paka.png" alt="パーカー"></a>
+    <a href="Parker-list.php"><img src="./image/zipparker-white.png" alt="パーカー" style="height: 300px; width: 300px;"></a>
         <p>パーカー</p>
-    </div>                                                            
+    </div>
+    
 </div>
 <h1>ピックアップ商品</h1>
+
 <div class="slider">
 <img src="image/tyun.png" width="500" height="500" alt="">
-<img src="image/ttt.png" width="500" height="500" alt="">
+<img src="image/kikuya.png" width="500" height="500" alt="">
 <img src="image/tyun.png" width="500" height="500" alt="">
 <img src="image/ttt.png" width="500" height="500" alt="">
 </div>
+
+
 <?php require 'footer.php'; ?>
