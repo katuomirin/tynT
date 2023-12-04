f6448b5cf22f15d5427711934bde3182750d0f95
n6-lu <2201130@s.asojuku.ac.jp>
2023-12-04 14:12:17 +0900

    Update Polo-shirt-list.php

--- shoppingsite/Polo-shirt-list.php
+++ shoppingsite/Polo-shirt-list.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <?php require 'header.php'; ?>
 <?php require 'db-connect.php'; ?>
 <link rel="stylesheet" href="./css/shohins.css">
@@ -12,20 +13,72 @@
     $pdo=new PDO($connect,USER,PASS);
     
     $sql = $pdo->prepare('select * from product where category = :category');
-    $sql->bindParam(':category', $T, PDO::PARAM_STR);
-    $sql->execute();
-    
-    echo '<div class="item">';
+        $sql->bindParam(':category', $T, PDO::PARAM_STR);
+        $sql->execute();
+        $user_id = $_SESSION['user']['id'];
+        $checkSql = $pdo->prepare('SELECT * FROM favorite WHERE user_id = ? AND product_id = ?');
+        
+        echo '<div class="item">';
             foreach ($sql as $row) {
                 $id = $row['id'];
                 echo '<div class="shohins">';
                 echo '<a href="T-details.php?id=', $id, '"><img class="img" alt="image" src="image/',$row['image'], '.png"></a>';
-                echo '<div class="choice-list" data-postid="', $id, '">
-                        <div class="checkbox heart"></div>
-                    </div>';
+                $checkSql->execute([$user_id, $id]);
+                if ($checkSql->rowCount() > 0) {
+                    echo '<div class="choice-list1" data-postid1="', $id, '">
+                            <div class="checkbox1 heart"></div>
+                          </div>';
+                } else {
+                    echo '<div class="choice-list" data-postid="', $id, '">
+                            <div class="checkbox heart"></div>
+                          </div>';
+                }
                 echo '<a href="T-details.php?id=', $id, '">', $row['name'], '</a>';
                 echo '<p class="price">', $row['price'], '</p></div>';
             }
+                echo   '<script>
+                    $(function() {
+                        var $favorite = $(\'.checkbox1\'), //お気に入りボタンセレクタ
+                        productId;
+                        $favorite.on(\'click\',function(e){
+                            //カスタム属性（postid）に格納された投稿ID取得
+                            productId = $(this).parents(\'.choice-list1\').data(\'postid1\'); 
+                            console.log("クリック前の処理");
+                            console.log("ID=" + productId);
+                            if (!$(this).hasClass("is-checked1")) {
+                                console.log("クリック前の処理");
+                                $.ajax({
+                                    type: "POST",
+                                    url: "favorite-insert.php",
+                                    data: {id: productId},
+                                    success: function(response) {
+                                        // レスポンスを処理する（必要に応じて）
+                                        console.log(response);
+                                    },
+                                    error: function(error) {
+                                        console.error(error);
+                                    }
+                                });
+                            }
+                            $(this).toggleClass("is-checked1");
+                            if ($(this).hasClass("is-checked1")) {
+                                console.log("クリック後の処理");
+                                $.ajax({
+                                    type: "POST",
+                                    url: "favorite-delete.php",
+                                    data: {id: productId},
+                                    success: function(response) {
+                                        // レスポンスを処理する（必要に応じて）
+                                        console.log(response);
+                                    },
+                                    error: function(error) {
+                                        console.error(error);
+                                    }
+                                });
+                            }
+                        });
+                    });
+                </script>';
             echo   '<script>
                     $(function() {
                         var $favorite = $(\'.checkbox\'), //お気に入りボタンセレクタ
@@ -37,6 +90,18 @@
                             console.log("ID=" + productId);
                             if (!$(this).hasClass("is-checked")) {
                                 console.log("クリック前の処理");
+                                $.ajax({
+                                    type: "POST",
+                                    url: "favorite-delete.php",
+                                    data: {id: productId},
+                                    success: function(response) {
+                                        // レスポンスを処理する（必要に応じて）
+                                        console.log(response);
+                                    },
+                                    error: function(error) {
+                                        console.error(error);
+                                    }
+                                });
                             }
                             $(this).toggleClass("is-checked");
                             if ($(this).hasClass("is-checked")) {
