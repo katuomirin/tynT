5535f3d3ee627c23a66cccf388d63d85c880e255
yamasita123 <2201209@s.asojuku.ac.jp>
2023-12-05 10:45:00 +0900

    Update T-details.php

--- shoppingsite/T-details.php
+++ shoppingsite/T-details.php
@@ -378,8 +378,8 @@ window.addEventListener('DOMContentLoaded',function(){
         echo '<form action="cart-incert.php" method="post">';
         echo '<input type="hidden" name="id" value="', $row['id'], '">';
         echo '<input type="hidden" name="name" value="', $row['name'], '">';
-        echo '<input type="hidden" name="processingFee" id="processingFee" value="0">'; // 各商品の加工費を保存するフィールド
         echo '<input type="hidden" name="price" value="', $row['price'], '">';
+        echo '<input type="hidden" name="processingFee" id="processingFee" value="0">'; // 各商品の加工費を保存するフィールド
         echo '<input type="hidden" name="subtotal" id="subtotalInput" value="0">'; // 各商品の小計を保存するフィールド
         echo '<input type="hidden" name="quantity" id="quantityOutput" value="0">'; // 各商品の数量を保存するフィールド
         echo '<p class="cart-botton"><input type="submit" value="カートに追加" onclick="addToCart();"></p>'; // カートに追加ボタン
