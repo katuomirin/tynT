0b83438df96e3754866ae767225f09f9bcdd86de
katuomirin <2003euph@gmail.com>
2023-12-06 13:23:11 +0900

    Update T-details.php

--- shoppingsite/T-details.php
+++ shoppingsite/T-details.php
@@ -453,7 +453,7 @@ window.addEventListener('DOMContentLoaded',function(){
         echo '<input type="hidden" name="id" value="', $row['id'], '">';
         echo '<input type="hidden" name="name" value="', $row['name'], '">';
         echo '<input type="hidden" name="price" value="', $row['price'], '">';
-        echo '<input type="hidden" name="processingFee" id="processingFee" value="0">'; // 各商品の加工費を保存するフィールド
+        echo '<input type="hidden" name="processingFee" id="processingFeeInput" value="0">'; // 各商品の加工費を保存するフィールド
         echo '<input type="hidden" name="subtotal" id="subtotalInput" value="0">'; // 各商品の小計を保存するフィールド
         echo '<input type="hidden" name="quantity" id="quantityOutput" value="0">'; // 各商品の数量を保存するフィールド
         echo '<p class="cart-botton"><input type="submit" value="カートに追加" onclick="addToCart();"></p>'; // カートに追加ボタン
@@ -468,6 +468,7 @@ window.addEventListener('DOMContentLoaded',function(){
     }
 
     // $rowの値をJavaScriptに渡す
+    /*
     echo '<script>';
     echo 'var rowPrice = ', $row['price'], ';';
     echo 'function setHiddenFields() {
@@ -490,7 +491,7 @@ window.addEventListener('DOMContentLoaded',function(){
                 return processingFee;
             }';
     echo '</script>';
-    
+    */
 
     // お気に入りのチェックボックスに対する jQuery コード
     echo '<script>';
