67618964b92e813ace7f6d49d8f41608b01d460b
n6-lu <2201130@s.asojuku.ac.jp>
2023-12-06 14:06:09 +0900

    Update mypage-change-output.php

--- shoppingsite/mypage-change-output.php
+++ shoppingsite/mypage-change-output.php
@@ -3,19 +3,24 @@
 <?php require 'header.php'; ?>
 <link rel="stylesheet" href="./css/mypage-change-output.css">
 <?php
-    $kana = $kanji = $email = $password = $birthday = $gender = $post_code
-    = $prefectures = $address1 = $address2 = $manshon = '';
-    $kana = $_POST['kana'];
-    $kanji = $_POST['kanji'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $birthday = $_POST['birthday'];
-    $gender = $_POST['gender'];
-    $post_code = $_POST['post_code'];
-    $prefectures = $_POST['prefectures'];
-    $address1 = $_POST['address1'];
-    $address2 = $_POST['address2'];
-    $manshon = $_POST['manshon'];
+
+$kana = $kanji = $email = $password = $birthday = $gender = $post_code
+= $prefectures = $address1 = $address2 = $manshon = '';
+$kana = $_POST['kana'];
+$kanji = $_POST['kanji'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$birthday = $_POST['birthday'];
+$gender = $_POST['gender'];
+$post_code = $_POST['post_code'];
+$prefectures = $_POST['prefectures'];
+$address1 = $_POST['address1'];
+$address2 = $_POST['address2'];
+$manshon = $_POST['manshon'];
+
+var_dump($_POST['kana']);
+
+exit;
 
 $pdo=new PDO($connect, USER, PASS);
 if(!empty($_POST['password'])){
@@ -31,6 +36,8 @@ if (isset($_SESSION['user'])){
     $sql=$pdo->prepare('select * from user where email=?');
     $sql->execute([$_POST['email']]);
 }
+
+
 if(empty($sql->fetchAll())){
     if(isset($_SESSION['user'])){
         if(!empty($hashedPassword)){
