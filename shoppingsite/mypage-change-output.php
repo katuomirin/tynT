aa5c866bc83033ba76570f3713c04ac2c546d392
n6-lu <2201130@s.asojuku.ac.jp>
2023-12-06 14:45:29 +0900

    Update mypage-change-output.php

--- shoppingsite/mypage-change-output.php
+++ shoppingsite/mypage-change-output.php
@@ -4,23 +4,19 @@
 <link rel="stylesheet" href="./css/mypage-change-output.css">
 <?php
 
-$kana = $kanji = $email = $password = $birthday = $gender = $post_code
-= $prefectures = $address1 = $address2 = $manshon = '';
+ $kana = $kanji = $email = $password = $birthday = $gender = $post_code
+ = $prefectures = $address1 = $address2 = $manshon = '';
+ 
 $kana = $_POST['kana'];
 $kanji = $_POST['kanji'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $birthday = $_POST['birthday'];
-$gender = $_POST['gender'];
 $post_code = $_POST['post_code'];
 $prefectures = $_POST['prefectures'];
 $address1 = $_POST['address1'];
 $address2 = $_POST['address2'];
 $manshon = $_POST['manshon'];
-
-var_dump($_POST['kana']);
-
-exit;
 
 $pdo=new PDO($connect, USER, PASS);
 if(!empty($_POST['password'])){
@@ -41,10 +37,10 @@ if (isset($_SESSION['user'])){
 if(empty($sql->fetchAll())){
     if(isset($_SESSION['user'])){
         if(!empty($hashedPassword)){
-        $sql=$pdo->prepare('update user set kana=?, kanji=?, birthday=?, gender=?, post_code=?, prefectures=?, address1=?, address2=?, manshon=?,'.
+        $sql=$pdo->prepare('update user set kana=?, kanji=?, birthday=?, post_code=?, prefectures=?, address1=?, address2=?, manshon=?,'.
                            'email=?, password=? where id=?');
         $sql->execute([
-            $_POST['kana'], $_POST['kanji'],$_POST['email'],$_POST['password'],$_POST['birthday'],$_POST['gender'],
+            $_POST['kana'], $_POST['kanji'],$_POST['email'],$_POST['password'],$_POST['birthday'],
             $_POST['post_code'],$_POST['prefectures'],$_POST['address1'],$_POST['address2'],$_POST['manshon'],
             $hashedPassword,$id]);
         }else{
@@ -54,13 +50,15 @@ if(empty($sql->fetchAll())){
             'id'=>$id, 'kana'=>$_POST['kana'],
             'kanji'=>$_POST['kanji'], 'email'=>$_POST['email'],
             'password'=>$_POST['password'], 'birthday'=>$_POST['birthday'],
-            'gender'=>$_POST['gender'], 'post_code'=>$_POST['post_code'],
-            'prefectures'=>$_POST['prefectures'], 'address1'=>$_POST['address1'],
+            'post_code'=>$_POST['post_code'],'prefectures'=>$_POST['prefectures'], 
+            'address1'=>$_POST['address1'],
             'address2'=>$_POST['address2'],
             'manshon'=>$_POST['manshon'],
             'password'=>$hashedPassword];
         echo 'お客様情報を更新しました。';
     }
 }
+
+var_dump[$_POST]
 ?>
 <?php require 'footer.php'; ?>
